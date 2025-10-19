4f0a9fd5a899a489f7089642c47b9f83bb407fa4
samuelgrojas <samuel.g.rojas@accenture.com>
2025-10-19 20:54:26 +0200

    Segunda parte Front

--- Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ Project/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-37mr64f34epz156f.us.auth0.com', // the auth0 domain prefix
+    audience: 'Coffee-Shop-Backend', // the audience set for the auth0 app
+    clientId: 'DFyUcE66SR5GtnxeV9lsOIQKBjU3loC0', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
